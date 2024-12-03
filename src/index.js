e1b0bdf7dc3324a7bd31032af7288db43bab0ec4
Madhu Bharadwaj <116439829+madhumaddy77@users.noreply.github.com>
2024-12-03 15:19:04 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App'; // Import the App component
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 ReactDOM.render(
-  <BrowserRouter basename="/dynamic-form-progress">
-  <App />
-</BrowserRouter>,
-document.getElementById('root')
+  <React.StrictMode>
+    <App /> {/* Render the App component */}
+  </React.StrictMode>,
+  document.getElementById('root')
 );
